53e48dcddef9e49166a2c4ff03be748585e372c6
Darkstorm <jason1997.jz@gmail.com>
2016-03-07 19:09:26 -0500

    added some basic things to main(nonfunctional so far)

---
+++ main.py
@@ -0,0 +1,46 @@
+import libtcodpy as libtcod
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+#this is where the font code line will be at
+
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'The trap of the 7 lords', False)
+
+while not libtcod.console_is_window_closed():
+    libtcod.console_set_default_foreground(0, libtcod.white)
+    libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+
+    libtcod.console_flush()
+
+    libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+
+playerx = SCREEN_WIDTH/2
+playery = SCREEN_HEIGHT/2
+
+def handle_keys():
+    global playerx, playery
+
+    #movement keys
+    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        playery -= 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        playery += 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        playerx -= 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        playerx += 1
+
+    key = key = libtcod.console_wait_for_keypress(True)
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True #exit game
+
+
