95a032440b0dcaab719748b24d21528bbcd12f31
Darkstorm <jason1997.jz@gmail.com>
2016-03-08 01:16:12 -0500

    added libtcod-mingw.dll and SDL.dll for windows use

--- main.py
+++ main.py
@@ -43,4 +43,16 @@ def handle_keys():
     elif key.vk == libtcod.KEY_ESCAPE:
         return True #exit game
 
+###     Code for map generation     ###
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
 
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+
+class Tile:
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
