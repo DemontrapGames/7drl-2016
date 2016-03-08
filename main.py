16ac1f720b683204ad323bdbe82032bc443210dc
Darkstorm <jason1997.jz@gmail.com>
2016-03-08 01:51:38 -0500

    added changes to main.py and is now functional

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import libtcodpy as libtcod
 
+#screen size
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
+
 LIMIT_FPS = 20
-
-libtcod.console_set_custom_font('terminal10x10_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'The trap of the 7 lords', False)
-
-while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
-
-    libtcod.console_flush()
-
-    libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
-
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
 
 def handle_keys():
     global playerx, playery
+
+    key = libtcod.console_wait_for_keypress(True)
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True #exit game
 
     #movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
@@ -35,13 +30,26 @@ def handle_keys():
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
         playerx += 1
 
-    key = libtcod.console_wait_for_keypress(True)
-    if key.vk == libtcod.KEY_ENTER and key.lalt:
-        #Alt+Enter: toggle fullscreen
-        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
-    elif key.vk == libtcod.KEY_ESCAPE:
-        return True #exit game
+libtcod.console_set_custom_font('terminal10x10_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'The trap of the 7 lords', False)
+libtcod.sys_set_fps(LIMIT_FPS)
+
+playerx = SCREEN_WIDTH/2
+playery = SCREEN_HEIGHT/2
+
+while not libtcod.console_is_window_closed():
+
+    libtcod.console_set_default_foreground(0, libtcod.white)
+    libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+
+    libtcod.console_flush()
+
+    libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+
+    exit = handle_keys()
+    if exit:
+        break
 
 ###     Code for map generation     ###
 MAP_WIDTH = 80
