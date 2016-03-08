b45f988a5d240448897775159272b8deba296ecd
Darkstorm <jason1997.jz@gmail.com>
2016-03-08 01:19:20 -0500

    added libtcod-mingw.dll and SDL.dll for windows use and font terminal10x10

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
-#this is where the font code line will be at
+libtcod.console_set_custom_font('terminal10x10_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'The trap of the 7 lords', False)
 
